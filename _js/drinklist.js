fab30ab01691aa6b50df716e51336592b7a4ec3b
Chris Bewick <chris@stompfrog.co.uk>
2011-12-19 22:15:34 +0000

    moving drinks rendering into a separate view

--- _js/drinklist.js
+++ _js/drinklist.js
@@ -1,166 +1,75 @@
 (function ($) {
   
-  Drink = Backbone.Model.extend({
-    //Create a model to hold friend atribute
-    name: 'Name unknown',
-    drink: 'Drink unknown',
-    milk: 'Milk unknown',
-    sugar: 'Sugar unknown',
-    cont: 'cont unknown'
-  });
-  
-  Drinks = Backbone.Collection.extend({
-    //This is our Friends collection and holds our Friend models
-    initialize: function (models, options) {
-      this.bind("add", options.view.renderDrink);
-      //Listen for new additions to the collection and call a view function if so
-    }
-  });
-  
-  AppView = Backbone.View.extend({
-    el: $("body"),
-    initialize: function () {
-      this.drinks = new Drinks( null, { view: this });
-      //Create a friends collection when the view is initialized.
-      //Pass it a reference to this view to create a connection between the two
-    },
-    events: {
-    	"submit #person-form":  "addDrink"
-   },
-   
-   template: _.template($('#person-template').html()),
-
-
-    renderDrink: function (model) {
-      //The parameter passed is a reference to the model that was added
-      
-      console.log(model.attributes);
-      
-      $('#drinklist').append(_.template($('#person-template').html(),model.attributes));
-
-    },
-    
-    
-    addDrink: function(e) {
-    	e.preventDefault();
-    	var data = { 
-		    name: $('#person-form #name').val(),
-		    drink: $('#person-form #drink').val(),
-		    milk: $('#person-form #milk').val(),
-		    sugar: $('#person-form #sugar').val(),
-		    cont: $('#person-form #cont').val()
+	Drink = Backbone.Model.extend({
+		//Create a model to hold friend atribute
+		name: 'Name unknown',
+		drink: 'Drink unknown',
+		milk: 'Milk unknown',
+		sugar: 'Sugar unknown',
+		cont: 'cont unknown'
+	});
+	
+	Drinks = Backbone.Collection.extend({
+		//This is our Friends collection and holds our Friend models
+		initialize: function (models, options) {
+		  //this.bind("add", options.view.renderDrink);
+		  //Listen for new additions to the collection and call a view function if so
 		}
-      	var drink_model = new Drink(data);
-	    this.drinks.add( drink_model );
-    },
+	});
+	
+	window.drinksList = new Drinks;
+	
+	
+	DrinkView = Backbone.View.extend({
 
-  });
-  
-  var appview = new AppView;
-})(jQuery);
+		tagName:  "li",
 
-
-/*
-
-$(function(){
-
-window.Person = Backbone.Model.extend({
-    defaults: {
-        name: 'Name unknown',
-        drink: 'Drink unknown',
-        milk: 'Milk unknown',
-        sugar: 'Sugar unknown',
-        cont: 'cont unknown'
-    },
-    initialize: function(){
-        console.log("Initialisting person - "+this.get("name"));
-    }
-});
-
-
-
-window.DrinkList = Backbone.Collection.extend({
-	model: Person,
-	url: '/person'
-});
-window.Drinks = new DrinkList;
-
-
-
-
-
- window.PersonView = Backbone.View.extend({
- 
- 	el : $('#drinklist'),
-
-    tagName:  "li",
-
-    template: _.template($('#person-template').html()),
-
-    initialize: function() {    
-      this.model.bind('change', this.render, this);
-      this.model.bind('destroy', this.remove, this);
-    },
-
-    render: function() {
-      $(this.el).html(this.template,this.model.toJSON());
-      return this;
-    },
-
-    remove: function() {
-      $(this.el).remove();
-    },
-
-    clear: function() {
-      this.model.destroy();
-    }
-
-  });
-
-
-
-
-
-
-
-  window.AppView = Backbone.View.extend({
-
-    el: $("body"),
-
-    events: {
-      "submit #person-form":  "addDrink"
-    },
-
-    initialize: function() {
-      Drinks.bind('add',  this.renderDrink, this);
-    },
-
-    addDrink: function(e) {
-    	e.preventDefault();
-    	
-    	var data = { 
-		    name: $('#person-form #name').val(),
-		    drink: $('#person-form #drink').val(),
-		    milk: $('#person-form #milk').val(),
-		    sugar: $('#person-form #sugar').val(),
-		    cont: $('#person-form #cont').val()
-		}
-		console.log(data);
+		template: _.template($('#person-template').html()),
 		
-		var view = new PersonView({model: Person});
-		console.log(view);
-		$("#drinklist").html(view.render().el+'boo');
-
-   	
-    },
-    
-    
+		initialize: function() {    
+			this.model.bind('change', this.render, this);
+		},
+		
+		render: function(model) {
+			$(this.el).html(this.template(this.model.toJSON()));
+			return this;
+		}
+			
+	});
+  
+	AppView = Backbone.View.extend({
+	
+		el: $("body"),
+		
+		initialize: function () {
+			drinksList.bind('add', this.addOne, this);
+		},
+		
+		events: {
+			"submit #person-form":  "addDrink"
+		},
+		    
+	    addDrink: function(e) {
+	    	e.preventDefault();
+	    	var data = { 
+			    name: $('#person-form #name').val(),
+			    drink: $('#person-form #drink').val(),
+			    milk: $('#person-form #milk').val(),
+			    sugar: $('#person-form #sugar').val(),
+			    cont: $('#person-form #cont').val()
+			}
+	      	var drink_model = new Drink(data);
+		    drinksList.add( drink_model );
+	    },
+	    	    
+		addOne: function(drink) {	
+			var view = new DrinkView({model: drink});
+			$('#drinklist').append(view.render().el);
+		}
+	    
+	    
 
   });
-
-  // Finally, we kick things off by creating the **App**.
-  window.App = new AppView;
+  var appview = new AppView;
   
-});
-
-*/
+})(jQuery);
